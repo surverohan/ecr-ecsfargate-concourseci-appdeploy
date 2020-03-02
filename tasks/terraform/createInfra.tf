c532721dc722f5a2c4d13363e3fce74458ff6cba
rohansurve rohansurve <rohansurve26@gmail.com>
2020-03-02 13:15:45 -0600

    test

--- tasks/terraform/createInfra.tf
+++ tasks/terraform/createInfra.tf
@@ -1,7 +1,7 @@
 provider "aws" { 
   region = "us-east-1"
   access_key = "AKIARD4KESXLKY4HA4WW"
-  secret_key = "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
+  secret_key = "5OjDdV1bhu2/Hic+dCsDGBef5Md6CNUd9I/vCZ5B"
 }
 
 resource "aws_s3_bucket" "bucket" {
