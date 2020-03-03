0ab1e4efb7a6cd0105efcdeb96eecc6b81fc2a50
rohansurve rohansurve <rohansurve26@gmail.com>
2020-03-03 09:12:10 -0600

    test

---
+++ tasks/terraform/infraprovider.tf
@@ -0,0 +1,7 @@
+# Specify the provider and access details
+
+provider "aws" { 
+  region = "us-east-1"
+  access_key = "AKIA5A6WR4FYPY2ATB4F"
+  secret_key = "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"
+}
